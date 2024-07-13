85ef85f86959e282b03d56488dfed04f59c8c2e2
aytuns <ayotunde.jerry@gmail.com>
2024-07-13 18:55:49 +0100

    terraform for aws

---
+++ README.md
@@ -0,0 +1,17 @@
+# Main.tf
+
+This file contains the main configuration for the Terraform project.
+
+## Overview
+
+The `main.tf` file serves as the entry point for defining the infrastructure resources and their configurations.
+
+## Contents
+
+Provisioning infrastructure on AWS
+
+## Usage
+
+1. Initialize Terraform: Run the `terraform init` command in the project directory to initialize Terraform and download the necessary provider plugins.
+
+2. Plan and Apply: Run the `terraform plan` command to preview the changes that Terraform will make to your infrastructure. Then, run `terraform apply` to apply those changes, and `terraform destroy` to clear all that was applied.
---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 4.16"
+        }
+  }
+    required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIA2UC3AXWMKB6OBXFZ"
+    secret_key = "m74ZEVxbyMk2Ae61qQ+uYhWF6nfnKHQKCyZI7Ziw"
+}
+
+resource "aws_instance" "example_server" {
+    ami = "ami-06f4fa348c295a6f6"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "mini_server-1"
+    }
+}
+
+resource "aws_ecr_repository" "ecr_1" {
+  name = "ecr-1"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
